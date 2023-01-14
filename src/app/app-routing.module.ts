f3b9cdade0db00c76aca1a1dd160240b90e484b9
Carla Girardini <carligir@gmail.com>
2023-01-13 19:31:51 -0500

    navbar and home components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Public routes
+  {path: 'home', component: HomeComponent},
+
+  // Default route
+  {path: '**', pathMatch: 'full', redirectTo: 'home'} // Default route: Home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
