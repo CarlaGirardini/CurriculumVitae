eb43834ee542d057c81893059ccbf4c83138f31c
Carla Girardini <carligir@gmail.com>
2023-01-14 22:17:45 -0500

    Experience component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './components/experience/experience.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   // Public routes
   {path: 'home', component: HomeComponent},
+  {path: 'experience', component: ExperienceComponent},
 
   // Default route
   {path: '**', pathMatch: 'full', redirectTo: 'home'} // Default route: Home
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
